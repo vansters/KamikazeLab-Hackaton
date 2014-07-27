4b66c9eb984a62029c2988b84df97fd0b6dd1dc3
Mario Andrade <mario@kamikazelab.com>
2014-07-27 03:34:48 -0500

    agregando la vista de mobiles.

--- assets/js/angular-config.js
+++ assets/js/angular-config.js
@@ -4,10 +4,21 @@
 
     angular.module('newSwipe').config(['$routeProvider',
         function($routeProvider) {
+        	
+            var md = new MobileDetect( $(window)[0].navigator.userAgent ),
+            	view = "";
+
+            if( md.phone() ){
+                view = 'homeController.html';
+            }else if( md.tablet() ){
+                view = 'home.html';
+            }else{
+                view = 'home.html';
+            }
 
         	$routeProvider.
         		when('/home', {
-        			templateUrl: '/views/home.html'
+        			templateUrl: '/views/' + view
         		}).
         		otherwise({
                 	redirectTo: '/'
