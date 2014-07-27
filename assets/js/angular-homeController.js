c9cfc948b711430ee1f8dbcd405be42c12a67bd3
Mario Andrade <mario@kamikazelab.com>
2014-07-26 22:38:39 -0500

    agragando carucel del categorias y noticias.

--- assets/js/angular-homeController.js
+++ assets/js/angular-homeController.js
@@ -2,21 +2,37 @@
 
     'use strict';
 
-    angular.module('newSwipe.home').controller('HomeController', ['$scope', '$http', function ($scope, $http) {
+    angular.module('newSwipe.home').controller('HomeController', ['$scope', '$http','$timeout',function ($scope, $http, $timeout) {
 
         // this.urlBase = 'http://feeds2.feedburner.com/thenextweb';
-        this.urlBase = 'http://feeds.mashable.com/Mashable';
+        this.mashableRSS = 'http://feeds.mashable.com/Mashable';
+        this.awwwarsRSS = 'http://feeds.feedburner.com/awwwards-sites-of-the-day';
         this.urlApiGoogle = '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=';
-        this.feedDataAuthor = {};
-        $scope.feedData = [];
-        $scope.loQueSea = [1,2,3,4,5];
 
-        $http.jsonp(this.urlApiGoogle + encodeURIComponent(this.urlBase))
+        $scope.categories = ['Tecnología', 'Programación', 'Diseño ', 'Estrategia'];
+
+        $scope.onBeforeChange =  function() {
+            console.log("Hola Mundo");
+        };
+        // function cambio () {
+        //     console.log("Hola");
+        // };
+        // $scope.cambio = function (hola) {
+            // console.log("Cambiando");
+        // };
+
+        $http.jsonp(this.urlApiGoogle + encodeURIComponent(this.mashableRSS))
             .success(function(data, status, headers, config) {
+                $scope.images = [];
+                $scope.titles = [];
+                $scope.dates = [];
+                $scope.descriptions = [];
                 var x = data.responseData.feed.entries;
                 _.each(x, function(element, index) {
-                    element.img = getImage(element.content);
-                    $scope.feedData.push(element);   
+                    $scope.images.push(getImage(element.content));
+                    $scope.titles.push(element.title);
+                    $scope.dates.push(element.publishedDate);
+                    $scope.descriptions.push(element.contentSnippet);
                 });
             });
 
